691d0023d0571a0489024eaffe6d32773767648c
geryan <geryan@users.noreply.github.com>
2025-03-14 16:39:13 +1100

    funny

---
+++ R/parse_all_surveys.R
@@ -0,0 +1,16 @@
+parse_all_surveys <- function() {
+  bind_rows(
+    parse_all_uom_surveys(),
+    parse_barometer(),
+    parse_all_doh_surveys()
+  ) %>%
+    mutate(
+      weekend_fraction = weekend_weight(date)
+    ) %>%
+    group_by(wave) %>%
+    mutate(
+      wave_date = median(date),
+      wave_duration = as.numeric(max(date) - min(date))
+    ) %>%
+    ungroup()
+}
